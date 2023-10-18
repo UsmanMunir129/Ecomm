7e19ffd4f4c043ef36ab74322392845f75e651e9
UsmanMunir129 <108785167+UsmanMunir129@users.noreply.github.com>
2023-10-18 18:40:44 +0500

    First Commit

--- src/screens/components/Dashboard/Endslider.js
+++ src/screens/components/Dashboard/Endslider.js
@@ -1,146 +1,88 @@
 import React from "react";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 import "./end.css";
 const images = [
   {
     link: "San Francisco – Oakland Bay Bridge, United States",
     imgPath:
-      "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
+      "https://images.unsplash.com/photo-1617127365659-c47fa864d8bc?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1374",
   },
   {
     link: "Bird",
     imgPath:
-      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+      "https://images.unsplash.com/photo-1617113930975-f9c7243ae527?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1374",
   },
   {
     link: "Bali, Indonesia",
     imgPath:
-      "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
+      "https://images.unsplash.com/photo-1617137968427-85924c800a22?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1374",
   },
   {
     link: "Goč, Serbia",
     imgPath:
-      "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
+      "https://images.unsplash.com/photo-1619533394727-57d522857f89?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1374",
+  },
+  {
+    link: "San Francisco – Oakland Bay Bridge, United States",
+    imgPath:
+      "https://images.unsplash.com/photo-1618886614638-80e3c103d31a?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1570",
+  },
+  {
+    link: "Bird",
+    imgPath:
+      "https://images.unsplash.com/photo-1609840170480-4c440bcd5d8f?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1374",
+  },
+  {
+    link: "Bali, Indonesia",
+    imgPath:
+      "https://images.unsplash.com/photo-1622497170185-5d668f816a56?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1374",
+  },
+  {
+    link: "Goč, Serbia",
+    imgPath:
+      "https://images.unsplash.com/photo-1617137984095-74e4e5e3613f?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1374",
   },
 ];
+
+const options = {
+  margin: 25,
+
+  responsiveClass: true,
+  dots: false,
+  autoplay: true,
+  smartSpeed: 1000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    400: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    700: {
+      items: 2,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+};
 const Endslider = () => {
   return (
-    <div className="container text-center my-3">
-      <div className="row mx-auto my-auto justify-content-center">
-        <div
-          id="recipeCarousel"
-          className="carousel slide"
-          data-bs-ride="carousel"
-        >
-          <div className="carousel-inner" role="listbox">
-            <div className="carousel-item active" interval={1000}>
-              <div className="col-md-3">
-                <div className="card">
-                  <div className="card-img">
-                    <img
-                      src="https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60"
-                      className="img-fluid"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div className="card">
-                  <div className="card-img">
-                    <img
-                      src="https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60"
-                      className="img-fluid"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div className="card">
-                  <div className="card-img">
-                    <img
-                      src="https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250"
-                      className="img-fluid"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div className="card">
-                  <div className="card-img">
-                    <img
-                      src="https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60"
-                      className="img-fluid"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <div className="col-md-3">
-                <div className="card">
-                  <div className="card-img">
-                    <img
-                      src="//via.placeholder.com/500x400/aba?text=5"
-                      className="img-fluid"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div className="card">
-                  <div className="card-img">
-                    <img
-                      src="//via.placeholder.com/500x400/fc0?text=6"
-                      className="img-fluid"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div className="card">
-                  <div className="card-img">
-                    <img
-                      src="//via.placeholder.com/500x400/aba?text=7"
-                      className="img-fluid"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div className="card">
-                  <div className="card-img">
-                    <img
-                      src="//via.placeholder.com/500x400/fc0?text=8"
-                      className="img-fluid"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
+    <div class="headcont">
+      <OwlCarousel className="owl-theme" loop {...options}>
+        {images.map((step, index) => (
+          <div class="item">
+            <a href={step.link}>
+              <img class="img" src={step.imgPath} alt="" />
+            </a>
           </div>
-          <a
-            className="carousel-control-prev bg-transparent w-auto"
-            href="#recipeCarousel"
-            role="button"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-          </a>
-          <a
-            className="carousel-control-next bg-transparent w-auto"
-            href="#recipeCarousel"
-            role="button"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-          </a>
-        </div>
-      </div>
+        ))}
+      </OwlCarousel>
     </div>
   );
 };
