c19d013b7716c666afab66e6ecf5650dca99b917
UsmanMunir129 <108785167+UsmanMunir129@users.noreply.github.com>
2024-01-08 16:43:30 +0500

    First Commit

--- src/screens/components/Dashboard/Homepage.js
+++ src/screens/components/Dashboard/Homepage.js
@@ -3,12 +3,16 @@ import Dashboard from "./Dashboard";
 import CarouselTop from "./CarouselTop";
 import Social from "./Social";
 import Endslider from "./Endslider";
+import ProductCarousel from "./ProductCarousel";
+import Slider2 from "./Slider2";
 
 const Homepage = () => {
   return (
     <div>
       <CarouselTop />
       <Dashboard />
+      <ProductCarousel />
+      <Slider2 />
       <Social />
       {/* <Trusted />
       <Services /> */}
---
+++ src/screens/components/Dashboard/ProductCarousel.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import "./productCarousel.css";
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 3,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 320 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
+const sliderImageUrl = [
+  //First image url
+  {
+    imgPath: [require("../../../assets/carouselimgs/11-1.png")],
+  },
+  //Second image url
+  {
+    imgPath: [require("../../../assets/carouselimgs/222-1.png")],
+  },
+  //Third image url
+  {
+    imgPath: [require("../../../assets/carouselimgs/333-1.png")],
+  },
+
+  //Fourth image url
+  {
+    imgPath: [require("../../../assets/carouselimgs/555-1.png")],
+  },
+];
+const ProductCarousel = () => {
+  return (
+    <div className="parent  bg-dark">
+      <Carousel
+        responsive={responsive}
+        autoPlay={true}
+        swipeable={true}
+        draggable={true}
+        infinite={true}
+        partialVisible={false}
+        dotListClass="custom-dot-list-style"
+      >
+        {sliderImageUrl.map((imageUrl, index) => {
+          return (
+            <div className="slider" key={index}>
+              <img src={imageUrl.imgPath} alt="movie" />
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
+};
+export default ProductCarousel;
